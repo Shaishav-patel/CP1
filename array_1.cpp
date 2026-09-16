fe94bcba81591cceb0f1a79c6be148d3570e1f27
Shaishav Patel <sp816535@gmail.com>
2026-09-16 10:39:14 +0530

    Add files via upload

---
+++ array_1.cpp
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main()
+{
+    int a[4],i;
+    printf("Enter 5 values ");
+    for(i=0;i<=4;i++)
+    {
+        scanf("%d",&a[i]);
+
+    }
+    for(i=0;i<=4;i++)
+   printf("%d\n",a[i]);
+    return 0;
+}
---
+++ array_2.cpp
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+    int a[9],i;
+    printf("Enter 10 values ");
+    for(i=0;i<=9;i++)
+    {
+        scanf("%d",&a[i]);
+
+    }
+   printf("%d\n",a[3]);
+   printf("%d\n",a[6]);
+   printf("%d\n",a[8]);
+    return 0;
+}
